a79890bf8c46feca981525f74015edf4742f8577
Leonides De Ocampo <leonides@deocampo.com>
2017-06-14 16:37:02 -0700

    Adding comments with values.

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,7 +3,7 @@ public class AddTwoNumbers {
         float num1 = (float) 1.8;   // The "(float)" "casts" the number as a single precision number
         float num2 = (float) 1.7;   // Without the cast, the Java compiler will interpret a number with a decimal point
                                     // as a double precision constant.
-        System.out.println("Sum of two numbers:"); // would output 3.2
+        System.out.println("Sum of two numbers :" + num1 + " and " + num2); // would output 3.2
         System.out.println(num1 + num2); // would output 3.2
     }
 }
